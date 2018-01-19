20f0bafc909dcebed5cab8702ef304802b7c5566
Shawn Patel <shawnpatel8@att.net>
2018-01-19 10:44:14 -0800

    Class Setup

---
+++ TypeFasterer.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class TypeFasterer here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TypeFasterer {
+    public static void main(String [] args) {
+       menu(); 
+    }
+    
+    private static void menu() {
+        System.out.println("Welcome to TyperFasterer!");
+        System.out.println("TyperFasterer tests your typing speed and accuracy.");
+    }
+    
+    
+}
